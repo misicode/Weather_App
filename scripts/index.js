75743846b428071a6abb3e3f81d17aa67b28c436
aleeMR <alee_1225@hotmail.com>
2022-07-13 19:56:46 -0500

    Actualizando interfaz y funcionalidad

--- scripts/index.js
+++ scripts/index.js
@@ -3,22 +3,26 @@
  ***********************************************************/
 
 let dateNow = new Date();
-const apiKey = "dcc96758227522fb2135712c910a87e1";
 
+// Selectores de elementos
 let place = document.getElementById("place");
-let temperature = document.getElementById("temperature");
-let icon = document.getElementById("icon");
-let climate = document.getElementById("climate");
 let daytime = document.getElementById("daytime");
 let humidity = document.getElementById("humidity");
 let wind = document.getElementById("wind");
+let icon = document.getElementById("icon");
+let temperature = document.getElementById("temperature");
+let climate = document.getElementById("climate");
 let city = document.getElementById("city");
+let forecast = document.querySelectorAll(".forecast-card");
+
+// Disparadores de eventos
 let btnSearch = document.getElementById("btnSearch");
-btnSearch.onclick = changeCity;
+btnSearch.onclick = updateWeather;
 let btnLocation = document.getElementById("btnLocation");
 btnLocation.onclick = getCurrentPosition;
 
-const days = {
+// Constantes
+const DAYS = {
     "0": "Sunday",
     "1": "Monday",
     "2": "Tuesday",
@@ -27,8 +31,7 @@ const days = {
     "5": "Friday",
     "6": "Saturday",
 };
-  
-const orderDays = {
+const ORDER_DAYS = {
     "1": "1st",
     "2": "2nd",
     "3": "3rd",
@@ -61,8 +64,7 @@ const orderDays = {
     "30": "30th",
     "31": "31st",
 };
-
-const months = {
+const MONTHS = {
     "0": "January",
     "1": "February",
     "2": "March",
@@ -76,8 +78,7 @@ const months = {
     "10": "November",
     "11": "December",
 };
-
-const iconMap = {
+const ICON_MAP = {
     "01d": "fa-solid fa-sun",
     "01n": "fa-solid fa-moon",
     "02d": "fa-solid fa-cloud-sun",
@@ -97,100 +98,112 @@ const iconMap = {
     "50d": "fa-solid fa-wind",
     "50n": "fa-solid fa-wind",
 };
+const API_KEY = "dcc96758227522fb2135712c910a87e1";
 
 
 /***********************************************************
  * FUNCIONES
  ***********************************************************/
 
+// Función de inicio
 function init() {
     formatDate();
     updateBackground();
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lima&appid=${ apiKey }&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lima&appid=${ API_KEY }&units=metric`;
     axios.get(apiUrl)
-        .then(showData)
+        .then(showWeather)
         .catch(err => console.error(err));
 }
 
-function changeCity(event) {
+// Función para actualizar la información del clima según la ciudad ingresada
+function updateWeather(event) {
     event.preventDefault();
-    let api = `https://api.openweathermap.org/data/2.5/weather?q=${ city.value }&appid=${ apiKey }&units=metric`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${ city.value }&appid=${ API_KEY }&units=metric`;
     axios.get(api)
-        .then(showData)
+        .then(showWeather)
         .catch(err => console.error(err));
 }
 
-function showData(response) {
-    console.log(response.data)
+// Función para mostrar la información del clima
+function showWeather(response) {
     place.innerHTML = `${ response.data.name }, ${ response.data.sys.country }`;
-    temperature.innerHTML = Math.round(response.data.main.temp);
-    icon.setAttribute("class", iconMap[response.data.weather[0].icon]);
-    climate.innerHTML = response.data.weather[0].main;
     daytime.innerHTML = `Last update: ${ formatTime() }`;
     humidity.innerHTML = `${ response.data.main.humidity } %`;
     wind.innerHTML = `${ response.data.wind.speed } km/h`;
+    icon.setAttribute("class", ICON_MAP[response.data.weather[0].icon]);
+    temperature.innerHTML = Math.round(response.data.main.temp);
+    climate.innerHTML = response.data.weather[0].main;
     updateForecast(response.data.coord);
 }
 
+// Función para actualizar el pronóstico del clima según la ciudad ingresada
 function updateForecast(coords) {
-    let api = `https://api.openweathermap.org/data/2.5/onecall?lat=${ coords.lat }&lon=${ coords.lon }&exclude=minutely,hourly&appid=${ apiKey }&units=metric`;
+    let api = `https://api.openweathermap.org/data/2.5/onecall?lat=${ coords.lat }&lon=${ coords.lon }&exclude=minutely,hourly&appid=${ API_KEY }&units=metric`;
     axios.get(api)
         .then(showForecast)
         .catch(err => console.error(err));
 }
 
+// Función para mostrar el pronóstico del clima
 function showForecast(response) {
-    console.log(response.data)
-    let timeWeek = document.querySelectorAll("li");
-    timeWeek.forEach( (element, i) => {
-        let day = element.querySelector("h3");
-        day.innerHTML = days[(dateNow.getDay() + (i+1)) % 7];
-        let tempMax = element.querySelector(".temp-max");
-        let tempMin = element.querySelector(".temp-min");
-        tempMax.innerHTML = `${ Math.round(response.data.daily[i].temp.max) }°C`;
-        tempMin.innerHTML = `${ Math.round(response.data.daily[i].temp.min) }°C`;
-        let icon = element.querySelector("i");
-        icon.setAttribute("class", iconMap[response.data.daily[i].weather[0].icon]);
-        let climate = element.querySelector(".clim-week");
-        climate.innerHTML = response.data.daily[i].weather[0].main;
+    forecast.forEach( (element, i) => {
+        (element.querySelector("h3")).innerHTML = DAYS[(dateNow.getDay() + (i+1)) % 7];
+        (element.querySelector(".temp-max")).innerHTML = `${ Math.round(response.data.daily[i].temp.max) }°`;
+        (element.querySelector(".temp-min")).innerHTML = `${ Math.round(response.data.daily[i].temp.min) }°`;
+        (element.querySelector("i")).setAttribute("class", ICON_MAP[response.data.daily[i].weather[0].icon]);
+        (element.querySelector(".clim-week")).innerHTML = response.data.daily[i].weather[0].main;
+        (element.querySelector(".humi-week")).innerHTML = `${ response.data.daily[i].humidity } %`;
+        (element.querySelector(".wind-week")).innerHTML = `${ response.data.daily[i].wind_speed } km/h`;
     });
 }
 
-function showPosition(position) {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl)
-        .then(showData)
-        .catch(err => console.error(err));
-}
-  
+// Función para obtener la ubicación actual
 function getCurrentPosition(event) {
     event.preventDefault();
     navigator.geolocation.getCurrentPosition(showPosition);
 }
 
+// Función para actualizar el pronóstico del clima según la ubicación actual
+function showPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${ latitude }&lon=${ longitude }&appid=${ API_KEY }&units=metric`;
+    axios.get(apiUrl)
+        .then(showWeather)
+        .catch(err => console.error(err));
+}
+
 // Función para darle formato a la fecha
 function formatDate() {
     let date = document.getElementById("date");
-    date.textContent = `${ days[dateNow.getDay()] }, ${ orderDays[dateNow.getDate()] } ${ months[dateNow.getMonth()] } ${ dateNow.getFullYear() }`;
+    date.textContent = `${ DAYS[dateNow.getDay()] }, ${ ORDER_DAYS[dateNow.getDate()] } ${ MONTHS[dateNow.getMonth()] } ${ dateNow.getFullYear() }`;
 }
 
-// Función para darle formato al tiempo
+// Función para darle formato a la hora actual
 function formatTime() {
-    if (dateNow.getHours() < 12)
-        return `${ dateNow.getHours() }:${ dateNow.getMinutes() } AM`
-    else
-        return `${ dateNow.getHours() }:${ dateNow.getMinutes() } PM`
+    let hours = dateNow.getHours();
+    let minutes = dateNow.getMinutes();
+
+    if (hours < 10)
+        hours = `0${ hours }`;
+    if (minutes < 10)
+        minutes = `0${ minutes }`;
+    
+    return (dateNow.getHours() < 12) ? `${ hours }:${ minutes } AM` : `${ hours }:${ minutes } PM`;
 }
 
+// Función para actualizar la imagen de fondo según la hora actual
 function updateBackground() {
-    if (dateNow.getHours() >= 19 || dateNow.getHours() <= 6)
-        (document.querySelector(".card")).style.background = "url('../public/wallpaper_evening.jpg') no-repeat center 80%";
-    else if (dateNow.getHours() <= 17)
-        (document.querySelector(".card")).style.background = "url('../public/wallpaper_morning.jpg') no-repeat center 80%";
-    else
-        (document.querySelector(".card")).style.background = "url('../public/wallpaper_afternoon.jpg') no-repeat center 80%";
+    if (dateNow.getHours() >= 19 || dateNow.getHours() <= 6) {
+        (document.querySelector(".card")).style.background = "url('../public/wallpaper_evening.jpg') no-repeat center 85%";
+        (document.querySelector("body")).style.backgroundColor = "#322a5d";
+    } else if (dateNow.getHours() <= 17) {
+        (document.querySelector(".card")).style.background = "url('../public/wallpaper_morning.jpg') no-repeat center 85%";
+        (document.querySelector("body")).style.backgroundColor = "#63416c";
+    } else {
+        (document.querySelector(".card")).style.background = "url('../public/wallpaper_afternoon.jpg') no-repeat center 85%";
+        (document.querySelector("body")).style.backgroundColor = "#402e4f";
+    }
 }
 
 window.onload = init;
